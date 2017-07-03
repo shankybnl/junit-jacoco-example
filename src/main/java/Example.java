a67660a573b2fc42aea5dba49929cb8894cdba3f
shanky <shanky.sharma@rivigo.com>
2017-07-03 14:26:48 +0530

    example

---
+++ src/main/java/Example.java
@@ -0,0 +1,14 @@
+
+public class Example {
+	
+	 public long someRandomMethod(int a, int b) {
+	    	
+	    	if (a > 10 && b > 10){
+	    		return a+b;
+	    	}
+	    	else{
+	    		return a-b;
+	    	}
+	       
+	    }
+}
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void verifySomeRandomMethod() {
+		Example junitTest = new Example();
+		junitTest.someRandomMethod(11, 12);
+	}
+}
